45fabe25958af6693e4a371d8b3c816c161e7bdf
truyenpn22 <phantruyen19722@gmail.com>
2023-02-08 21:57:16 +0700

    up animation

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-$(document).ready(function() {
-      setTimeout(function() {
-          $('body').addClass('loaded');
-      }, 1500);
+$(document).ready(function () {
 
-    //AOS
-    AOS.init();
-   
-   });
+  //loading JS
+  setTimeout(function () {
+    $('body').addClass('loaded');
+  }, 1500);
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function () {
+    $('html, body, #').animate({
+      scrollTop: 0
+    }, 1000, 'easeInOutExpo');
+    return false;
+  });
+
+
+  //AOS
+  AOS.init();
+
+});
